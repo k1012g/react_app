0ced18ac0286e94d3db262b586a33d943189f035
k1012g <kuri.ladybaby1012@gmail.com>
2020-02-24 17:42:25 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,80 @@ import Rect from './Rect';
 import './App.css';
 
 class App extends Component {
+  data = [
+    "This is list sample.",
+    "これはリストのサンプルです。",
+    "配列をリストに変換します。"
+  ];
+
+  msgStyle = {
+    color: "#900",
+    fontSize: "20pt",
+    margin: "20px 0px",
+    padding: "5px"
+  };
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: this.data
+    };
+  };
+
   render(){
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="#6f9" r="50" />
-      <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
-      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
+      <h2 style={this.msgStyle}>show list.</h2>
+      <List title="サンプル・リスト" data={this.data} />
     </div>;
-  }
+  };
 }
 
+class List extends Component {
+  number = 1;
+
+  title = {
+    color: "blue",
+    fontSize: "20pt",
+    fontWeight: "bold"
+  };
+
+  render(){
+    let data = this.props.data;
+    return (
+      <div>
+        <p style={this.title}>{this.props.title}</p>
+          <ul>
+            {data.map((item) =>
+              <Item number={this.number++} value={item} key={this.number} />
+            )}
+          </ul>
+      </div>
+    );
+  };
+};
+
+class Item extends Component {
+  li = {
+    listStyleType: "square",
+    color: "#06",
+    fontSize: "16pt",
+    margin: "0px",
+    padding: "0px"
+  };
+
+  num = {
+    fontWeight: "bold",
+    color: "red"
+  };
+
+  render(){
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}]</span>
+        {this.props.value}
+      </li>
+    );
+  };
+};
 export default App;
