3abfa2112581dba98f9a3f32bd740944698c9c17
k1012g <kuri.ladybaby1012@gmail.com>
2020-02-25 17:35:32 +0900

    memo application added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import MemoStore from './memo/Store';
 // import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
 
 
 // If you want your app to work offline and load faster, you can change
@@ -12,33 +12,8 @@ import { Provider } from 'react-redux';
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-let state_value = {
-	counter: 0,
-	message: "COUNTER"
-};
-
-function counter(state = state_value, action){
-	switch (action.type){
-		case 'INCREMENT':
-		return {
-			counter: state.counter + 1,
-			message: "INCREMENT"
-		};
-		case 'DECREMENT':
-		return {
-			counter: state.counter - 1,
-			message: "DECREMENT"
-		};
-		default:
-		return state;
-	};
-};
-
-let store = createStore(counter);
-
-
 ReactDOM.render(
-	<Provider store={store}>
+	<Provider store={MemoStore}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
---
+++ src/memo/Item.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Item extends Component {
+	th = {
+		backgroundColor: "blue",
+		color: "white",
+		fontSize: "14pt",
+		padding: "5px 10px",
+		width: "50px"
+	};
+
+	td = {
+		backgroundColor: "white",
+		border: "1px solid lightblue",
+		color: "darkblue",
+		fontSize: "14pt",
+		minWidth: "300px",
+		padding: "5px 10px"
+	};
+
+	data = {
+		backgroundColor: "white",
+		border: "1px solid lightblue",
+		color: "darkblue",
+		fontSize: "14pt",
+		padding: "5px 10px",
+		width: "80px"
+	};
+
+	render(){
+		let d = this.props.value.created;
+		let f = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+		return (
+			<tr>
+				<th style={this.th}>
+					No, {this.props.index}
+				</th>
+				<td style={this.td}>
+					{this.props.value.message}
+				</td>
+				<td style={this.data}>
+					{f}
+				</td>
+			</tr>
+		);
+	};
+};
+
+export default connect()(Item);
